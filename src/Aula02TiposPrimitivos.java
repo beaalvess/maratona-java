ad59022384226f8465ba110d4a0a051e9ef0f928
Triz <beatrizalvesctt@gmail.com>
2024-04-09 15:35:51 -0300

    Aula 02 - Tipos primitivos

---
+++ src/Aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+public class Aula02TiposPrimitivos {
+public static void main (String[] args) {
+    //int, string, boolean, char, double, float, byte, short & long.
+
+    int year = 2024;
+    String name = "Triz";
+    boolean curso = true;
+    boolean falso = false;
+    float example = 14.9F;
+    double s = 2500.0;
+    short idade = 19;
+    long num = (long) 155.89;
+    char caractere = 'B';
+
+    System.out.println("year");
+    System.out.println("name");
+    System.out.println("caractere");
+
+}
+}
